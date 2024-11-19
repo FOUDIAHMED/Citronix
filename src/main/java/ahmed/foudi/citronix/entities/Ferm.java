7f06d00c545997d8766300061160760ac2dc5a59
ahmed <foudiahmed82@gmail.com>
2024-11-19 17:07:20 +0100

    TEST

--- src/main/java/ahmed/foudi/citronix/entities/Ferm.java
+++ src/main/java/ahmed/foudi/citronix/entities/Ferm.java
@@ -1,8 +1,14 @@
 package ahmed.foudi.citronix.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Ferm {
-    @id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
