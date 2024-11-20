fa145b3475026a7b89aee97a61c8d922b40950f9
ahmed <foudiahmed82@gmail.com>
2024-11-20 09:57:28 +0100

    Tree dao

---
+++ src/main/java/ahmed/foudi/citronix/dao/TreeDAO.java
@@ -0,0 +1,9 @@
+package ahmed.foudi.citronix.dao;
+
+import ahmed.foudi.citronix.entities.Tree;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TreeDAO extends JpaRepository<Tree, Long> {
+}
