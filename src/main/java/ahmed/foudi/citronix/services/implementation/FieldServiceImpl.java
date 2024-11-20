2eeaf929d4bdbbc1b7258250d3c508301ca920e5
ahmed <foudiahmed82@gmail.com>
2024-11-20 09:57:05 +0100

    Field service

---
+++ src/main/java/ahmed/foudi/citronix/services/implementation/FieldServiceImpl.java
@@ -0,0 +1,6 @@
+package ahmed.foudi.citronix.services.implementation;
+
+import ahmed.foudi.citronix.services.interfaces.FieldServiceI;
+
+public class FieldServiceImpl implements FieldServiceI {
+}
